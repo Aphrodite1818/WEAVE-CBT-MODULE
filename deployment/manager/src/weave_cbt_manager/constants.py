63b08aacd4023c38403faad7bb051c1cf86a4101
Aphrodite1818 <taiwoayimora623@gmail.com>
2026-09-19 13:03:38 +0100

    reduce manager constants to scaffold

--- deployment/manager/src/weave_cbt_manager/constants.py
+++ deployment/manager/src/weave_cbt_manager/constants.py
@@ -1,18 +1,1 @@
-"""Shared constants for the WEAVE CBT Manager."""
-
-APP_NAME = "WEAVE CBT Manager"
-APP_VERSION = "0.1.0"
-
-DEFAULT_WEAVE_IMAGE = "ghcr.io/aphrodite1818/weave-cbt-module:deployment-test"
-DEFAULT_WEAVE_API_BASE_URL = "https://api.weavecloudspace.com"
-DEFAULT_POSTGRES_USER = "weave"
-DEFAULT_POSTGRES_DB = "weave_cbt"
-DEFAULT_SERVER_URL = "http://localhost"
-
-RUNTIME_DIR_NAME = "WeaveCBT"
-DEPLOYMENT_DIR_NAME = "runtime"
-LOG_DIR_NAME = "logs"
-BACKUP_DIR_NAME = "backups"
-STATE_FILE_NAME = "manager-state.json"
-ENV_FILE_NAME = ".env"
-COMPOSE_FILE_NAME = "compose.yaml"
+"""Scaffold for shared WEAVE CBT Manager constants."""
